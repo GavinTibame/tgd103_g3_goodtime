0e9c26a589f58c076ddb1b1396a2fb43d3eef5df
diablo51030 <black5103000@gmail.com>
2022-12-26 12:15:07 +0800

    update index without calendar

---
+++ js/index.js
@@ -0,0 +1,4 @@
+const nextIcon = '<img src="../img/index/nextarrow.png" alt="">'
+const prvIcon = '<img src="../img/index/prvarrow.png">'
+
+const shopNextIcon = '<img src="../img/index/prvarrow.png">'
