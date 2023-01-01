7dc98023172af7f773a5bc4e16f3bbdb16713ab7
GavinTibame <tibamehui@gmail.com>
2023-01-01 14:51:09 +0800

    Gulp搬家

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const nextIcon = '<img src="../img/index/nextarrow.png" alt="">'
-const prvIcon = '<img src="../img/index/prvarrow.png">'
-
-const shopNextIcon = '<img src="../img/index/shopprvarrow.png">'
-
-    
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 const nextIcon = '<img src="../img/index/nextarrow.png" alt="">'
 const prvIcon = '<img src="../img/index/prvarrow.png">'
 
-const shopNextIcon = '<img src="../img/index/prvarrow.png">'
+const shopNextIcon = '<img src="../img/index/shopprvarrow.png">'
+
+    
