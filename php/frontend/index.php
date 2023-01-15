fcff4654974e5c796c1fed35a842cf177e6fc8e8
diablo51030 <black5103000@gmail.com>
2023-01-15 17:08:54 +0800

    index php

---
+++ php/frontend/index.php
@@ -0,0 +1,36 @@
+<?php
+
+    include("../connect.php");
+    include("../verify.php");
+    
+    //輪播
+    $pdo = connectDB();
+    $sql = "SELECT CONTENT, MAIN_PHOTO
+            FROM GOODTIME.f_index_1
+            limit 0, 5";
+    $statement = $pdo->prepare($sql);
+    $statement->execute();
+    $data["slider"] = $statement->fetchAll();
+    
+    //news
+    $pdo = connectDB();
+    $sql = "SELECT TITLE, DATE(EDIT_DATE) as EDIT_DATE, `DESC`
+            FROM GOODTIME.f_index_2
+            -- 依照EDIT_DATE升冪排列
+            order by EDIT_DATE desc 
+            limit 0, 4";
+    $statement = $pdo->prepare($sql);
+    $statement->execute();
+    $data["news"] = $statement->fetchAll();
+
+    //日曆
+    $pdo = connectDB();
+    $sql = "SELECT TITLE, `START_DATE`, END_DATE
+            FROM f_expo";
+    $statement = $pdo->prepare($sql);
+    $statement->execute();
+    $data["calendar"] = $statement->fetchAll();
+
+
+    echo json_encode($data);
+?>
