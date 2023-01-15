b16596f2ea2db003fe7f8dee018f0cd50ba118c2
GavinTibame <tibamehui@gmail.com>
2023-01-15 09:37:47 +0800

    會員中心API︰memberCenter.php已可初始化會員中心

--- php/frontend/initCenter.php
+++ php/frontend/initCenter.php
@@ -1,9 +1,9 @@
 <?php
     include("../connect.php");
-
+    
     function initCenter($mid){
         $pdo = connectDB();
-        
+
         function selectSQL($pdo, $sql, $mid){
             // 進出SQL用語法
             $statement = $pdo->prepare($sql);
@@ -12,12 +12,12 @@
             return $statement->fetchAll();
         }
 
-        $sql = "SELECT LOCATION FROM ADDRESS 
+        $sql = "SELECT ID, LOCATION FROM ADDRESS 
         WHERE FK_ADDRESS_MEMBER_ID = :mid";
 
         $data["address"] = selectSQL($pdo, $sql, $mid);
 
-        $sql = "SELECT PO.ID, PO.FREIGHT, PO.STATUS, PO.CREATE_DATE, POD.ID, POD.UNIT_PRICE, POD.QTY, 
+        $sql = "SELECT PO.ID , PO.FREIGHT, PO.STATUS, PO.CREATE_DATE, POD.ID, POD.UNIT_PRICE, POD.QTY, 
         PRODUCT.PRODUCT_NAME, PRODUCT.MAIN_PHOTO, 
         SPEC.SPEC
         FROM PO, PO_DETAIL POD, 
@@ -29,6 +29,27 @@
 
         $data["po"] = selectSQL($pdo, $sql, $mid);
 
+        // $sql = "SELECT ADDRESS.ID, ADDRESS.LOCATION, PO.ID, PO.FREIGHT, PO.STATUS, PO.CREATE_DATE, POD.ID, POD.UNIT_PRICE, POD.QTY, 
+        // PRODUCT.PRODUCT_NAME, PRODUCT.MAIN_PHOTO, 
+        // SPEC.SPEC
+        // FROM PO, PO_DETAIL POD, 
+        // PRODUCT, PRODUCT_SPEC SPEC, ADDRESS
+        // WHERE PO.FK_PO_MEMBER_ID = :mid
+        // AND ADDRESS.FK_ADDRESS_MEMBER_ID = PO.FK_PO_MEMBER_ID
+        // AND PO.ID = POD.FK_PO_DETAIL_PO_ID
+        // AND POD.FK_PO_DETAIL_PRODUCT_ID = PRODUCT.ID
+        // AND POD.FK_PO_PRODUCT_SPEC_ID = SPEC.ID";
+
+        // $test = selectSQL($pdo, $sql, $mid);
+
+        // // print_r($test[0]);
+        // foreach($test[0] as $row){
+        //     print_r($row);
+        //     // if ($index <= 1){
+        //     //     $data["po"] = $row;
+        //     // }
+        // }
+        // print_r($data["po"]);
         echo json_encode($data);
     }
 ?>
--- php/frontend/memberCenter.php
+++ php/frontend/memberCenter.php
@@ -1,24 +1,34 @@
 <?php
     include("../verify.php");
     include("initCenter.php");
+    include("newAddress.php");
 
     $mid = isMemberLogin();
     $mid = "5";
+    
+    
+    
     if ($mid == ""){
         echo "未登入會員";
     }else{
         // $action = $_POST["action"];
-        $action = null;
+        $action = "";
         switch($action){
-            case "address":
+            case "newAddress":
+                $newAddress = $_POST["address"]; 
+                // $newAddress = "台北市松山區敦化北路100號2樓"; 
+                newAddress($mid, $newAddress);
+                echo initCenter($mid);
+                break;
+            case "editAddress":
+
+                echo initCenter($mid);
+                break;
+            case "delAddress":
 
                 echo initCenter($mid);
                 break;
             case "password":
-
-                echo initCenter($mid);
-                break;
-            case "orderDetail":
                 
                 echo initCenter($mid);
                 break;
