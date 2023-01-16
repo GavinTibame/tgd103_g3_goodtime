5c1218d46a6ad793902701c042e16ff2d87f812d
1Kynechinling0618 <winds4658@gmail.com>
2023-01-16 09:04:00 +0800

    no message

--- src/js/new.js
+++ src/js/new.js
@@ -1,95 +1,183 @@
 const createApp = Vue.createApp({
-      data(){
-        return{
-            current: 1,
-            paginate: 5,
-            paginate_total: 0,
-            status_filter: '',
-            currentList:[],
-            tabpanels: [
-                { time: '2011-11-29',
-                  universe: '園區公告',
-                  message:'B展覽園區維修至12:00', 
-                  url:'http://localhost:5500/src/html/news_detail.html'
-                },
-                { time: '2011-11-29',
-                universe: '園區公告',
-                message:'B展覽園區維修至12:00', 
-                url:'http://localhost:5500/src/html/news_detail.html'
-              },
-              { time: '2011-11-29',
-              universe: '園區公告',
-              message:'B展覽園區維修至12:00', 
-              url:'http://localhost:5500/src/html/news_detail.html'
-            },
-            { time: '2011-11-29',
-            universe: '展演資訊',
-            message:'B展覽園區維修至12:00', 
-            url:'http://localhost:5500/src/html/news_detail.html'
-          },
-          { time: '2011-11-29',
-          universe: '其他消息',
-          message:'B展覽園區維修至12:00', 
-          url:'http://localhost:5500/src/html/news_detail.html'
+  data() {
+    return {
+      current: 1,
+      paginate: 4,
+      paginate_total: 0,
+      status_filter: "",
+      currentList: [],
+      tabpanels: [
+        {
+          time: "2011-11-29",
+          universe: "園區公告",
+          message: "B展覽園區維修至12:00",
+          url: "http://localhost:5500/src/html/news_detail.html",
         },
-        { time: '2011-11-29',
-        universe: '其他消息',
-        message:'B展覽園區維修至12:00', 
-        url:'http://localhost:5500/src/html/news_detail.html'
-      },
-      { time: '2011-11-29',
-      universe: '展演資訊',
-      message:'B展覽園區維修至12:00', 
-      url:'http://localhost:5500/src/html/news_detail.html'
+        {
+          time: "2011-11-29",
+          universe: "園區公告",
+          message: "C展覽園區廁所施工",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-29",
+          universe: "園區公告",
+          message: "A展覽園區廠商施工",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2022-11-11",
+          universe: "展演資訊",
+          message: "漫畫展延期至12/06",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2022-11-12",
+          universe: "展演資訊",
+          message: "《Lindsayooo》新興插畫家展",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-13",
+          universe: "展演資訊",
+          message: "《崖下的波妞》十周年特展",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-14",
+          universe: "展演資訊",
+          message: "《ヒグチユウコ》台灣限定特展",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-27",
+          universe: "其他消息",
+          message: "C展覽園館招租中",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-28",
+          universe: "其他消息",
+          message: "培可先生營業時間延長至24:00",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-29",
+          universe: "其他消息",
+          message: "線上訂購享9折",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-29",
+          universe: "其他消息",
+          message: "B展覽園區維修至12:00",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-29",
+          universe: "其他消息",
+          message: "線上訂購享9折",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-29",
+          universe: "其他消息",
+          message: "線上訂購享9折",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-29",
+          universe: "其他消息",
+          message: "線上訂購享9折",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-29",
+          universe: "其他消息",
+          message: "線上訂購享9折",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-29",
+          universe: "其他消息",
+          message: "線上訂購享9折",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-29",
+          universe: "其他消息",
+          message: "線上訂購享9折",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-29",
+          universe: "其他消息",
+          message: "線上訂購享9折",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-29",
+          universe: "其他消息",
+          message: "線上訂購享9折",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+        {
+          time: "2011-11-29",
+          universe: "其他消息",
+          message: "線上訂購享9折",
+          url: "http://localhost:5500/src/html/news_detail.html",
+        },
+      ],
+    };
+  },
+  created() {
+    this.paginate_total = this.currentList.length/this.paginate;
+  },
+  mounted(){
+  //  console.log('hi');
+  },
+  updated(){
+    this.updatePaginate();
+  },
+  methods: {
+    getData(){
+        this.currentList  = this.tabpanels
+        // this.setStatus('')
     },
-    { time: '2011-11-29',
-    universe: '展演資訊',
-    message:'B展覽園區維修至12:00', 
-    url:'http://localhost:5500/src/html/news_detail.html'
-    },
-    { time: '2011-11-29',
-    universe: '其他消息',
-    message:'B展覽園區維修至12:00', 
-    url:'http://localhost:5500/src/html/news_detail.html'
-    },
-    { time: '2011-11-29',
-    universe: '其他消息',
-    message:'B展覽園區維修至12:00', 
-    url:'http://localhost:5500/src/html/news_detail.html'
-    },
-    { time: '2011-11-29',
-    universe: '其他消息',
-    message:'B展覽園區維修至12:00', 
-    url:'http://localhost:5500/src/html/news_detail.html'
-    }
-        ]
-        }
-},methods: {
     setPaginate(i) {
       if (this.current == 1) {
         return i < this.paginate;
-      }
-      else {
-        return (i >= (this.paginate * (this.current - 1)) && i < (this.current * this.paginate));
+      } else {
+        return (
+          i >= this.paginate * (this.current - 1) &&
+          i < this.current * this.paginate
+        );
       }
     },
     setStatus(status) {
       this.status_filter = status;
-      this.currentList = this.tabpanels.filter((news)=> {
-        if(this.status_filter == ""){
-            this.currentList = this.tabpanels;
-        }else{
-            return news.universe == this.status_filter;
+      this.currentList = this.tabpanels.filter((news) => {
+        if (this.status_filter == "") {
+          this.currentList = this.tabpanels;
+        } else {
+          return news.universe == this.status_filter;
         }
       });
-      console.log(this.currentList);
+    //   console.log(this.currentList);
+      //   this.currentList.forEach((current)=>console.log(current));
     },
     updateCurrent(i) {
       this.current = i;
     },
     updatePaginate() {
       this.current = 1;
-      this.paginate_total = Math.ceil(document.querySelectorAll('tbody tr').length/this.paginate);
-    }
-  }
+      this.paginate_total = Math.ceil(
+        document.querySelectorAll(".tab-panel li").length / this.paginate
+      );
+      // console.log(Math.ceil(document.querySelectorAll(".tab-panel li").length / this.paginate));
+      console.log(this.paginate_total);
+      // console.log(this.currentList);
+    },
+  },
+
 }).mount("#news");
