0a9fecfcccb61ac7f95b9fdaabde15ea14280f24
diablo51030 <black5103000@gmail.com>
2023-01-18 18:45:43 +0800

    index 修改

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,23 @@ const shopNextIcon = '<img src="../img/index/shopprvarrow.png">'
 $(() => {
     $("#footer").load("footer.html");
 });
+
+
+//Calendar event color
+// $(document).ready(function () {
+//   let box = document.querySelectorAll('.fc-event');
+//   let colorArray = ['red','gray','blue','green'];
+
+//   for(let i = 0; i < colorArray.length ; i++){
+//       console.log(i);
+//       box[i].style.backgroundColor = colorArray[i];
+//       box[i].style.borderColor = colorArray[i];  
+//   }
+// });
+
+
+
+
+    
+
+
