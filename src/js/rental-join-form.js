55cad41df3e3e6417b1892ec147ff535f6147b00
evatung777889 <qwer777889@gmail.com>
2023-01-28 16:04:11 +0800

    rental_form表單 PHP與資料庫串接完成

--- src/js/rental-join-form.js
+++ src/js/rental-join-form.js
@@ -1,4 +1,4 @@
-console.log("bbbbb")
+console.log("ssss");
 /*------------------------檔案上傳-----------------------*/
 function file() {
   var that = this;
@@ -32,22 +32,22 @@ const rentalJoinFormApp = Vue.createApp({
       activity__name: "",
       applicant__unit: "",
       principal__name: "",
-      unit__spacing:"",
-      Contact__name:"",
-      Contact__phone:"",
-      nature__activity:[],
-      place__spacing:"",
-      ornamental__nature:"",
-      other__demands:[],
-      Opinion:"",
-      date_pick__from:"",
-      date_pick__to:"",
+      unit__spacing: "",
+      Contact__name: "",
+      Contact__phone: "",
+      nature__activity: [],
+      place__spacing: "",
+      ornamental__nature: "",
+      other__demands: [],
+      Opinion: "",
+      date_pick__from: "",
+      date_pick__to: "",
     };
   },
   methods: {
-    demands_change(){
-      if(this.other__demands.includes('無代售需求')){
-        this.other__demands=['無代售需求']
+    demands_change() {
+      if (this.other__demands.includes("無代售需求")) {
+        this.other__demands = ["無代售需求"];
       }
     },
 
@@ -63,45 +63,84 @@ const rentalJoinFormApp = Vue.createApp({
       }
 
       if (this.principal__name == "") {
-        alert("請負責人姓名");
+        alert("請輸入負責人姓名");
         return false;
       }
+
+      if (this.unit__spacing == "") {
+        alert("請選擇您的單位類別");
+        return false;
+      }
+
+      if (this.Contact__name == "") {
+        alert("請輸入您的聯絡人姓名");
+        return false;
+      }
+
+      if (this.Contact__phone == "") {
+        alert("請輸入您的聯絡人電話");
+        return false;
+      }
+      if (this.place__spacing == "") {
+        alert("請選擇您的場地");
+        return false;
+      }
+
+      if (this.date_pick__from == "") {
+        alert("請選擇活動開始時間");
+        return false;
+      }
+
+      if (this.date_pick__to == "") {
+        alert("請選擇活動結束時間");
+        return false;
+      }
+
+      if (this.nature__activity == "") {
+        alert("請選擇您的活動性質");
+        return false;
+      }
+
+      if (this.ornamental__nature == "") {
+        alert("請選擇您的觀賞性質");
+        return false;
+      }
+
+      if (this.other__demands == "") {
+        alert("請選擇其他需求");
+        return false;
+      }
+
       // axios.post('url') = 我們要獲取的API，會回傳一個 Promise 物件
       axios
-        .post("../../php/frontend/login.php", {
-          user_email: this.user_email,
-          user_password: this.user_password,
+        .post("../../php/frontend/rentalForm.php", {
+          activity__name: this.activity__name,
+          applicant__unit: this.applicant__unit,
+          principal__name: this.principal__name,
+          unit__spacing: this.unit__spacing,
+          Contact__name: this.Contact__name,
+          Contact__phone: this.Contact__phone,
+          nature__activity: this.nature__activity,
+          place__spacing: this.place__spacing,
+          ornamental__nature: this.ornamental__nature,
+          other__demands: this.other__demands,
+          Opinion: this.Opinion,
+          date_pick__from: this.date_pick__from,
+          date_pick__to: this.date_pick__to,
         })
 
         // then :處理 Promise返回的結果
         .then((res) => {
-          if (res.data === "登入成功") {
-            alert(res.data);
+          if (res.status === 200) {
+            alert("送出成功");
             // console.log(res);
-            window.location.href = "../html/memberCenter.html";
+            window.location.href = '../html/rental_join_form.html';
           } else {
             alert(res.data);
           }
         })
         //catch:抓取Promise 上異常
-        .catch((err) => console.log("[login error]", err));
-    },
-    handleSubmit() {
-      console.log("活動名稱:", this.applicant__unit);
-      console.log("申請單位:", this.activity__name);
-      console.log("負責人姓名:", this.applicant__unit);
-      console.log("單位類別:", this.unit__spacing);
-      console.log("聯絡人姓名:", this.Contact__name);
-      console.log("聯絡人電話:", this.Contact__phone);
-      console.log("選擇場地:", this.place__spacing);
-      console.log("活動開始時間:", this.date_pick__from);
-      console.log("活動開始時間:", this.date_pick__to);
-      console.log("活動性質:", this.nature__activity);
-      console.log("觀賞性質:", this.ornamental__nature);
-      console.log("其他需求:", this.other__demands);
-      console.log("意見回饋:", this.Opinion);
+        .catch((err) => console.log("[rental error]", err));
     },
   },
 }).mount("#rental_join_form");
-
-
