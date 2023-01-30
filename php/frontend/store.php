54efff2f66b38db8f16dc5aa73463f965fa841ac
1Kynechinling0618 <winds4658@gmail.com>
2023-01-30 15:50:26 +0800

    在更新store.js php

--- php/frontend/store.php
+++ php/frontend/store.php
@@ -2,7 +2,7 @@
 include("../connect.php");
 
 function getStoreList($pdo){
-    $sql = "SELECT ID , MAIN_PHOTO , PRODUCT_NAME , PRICE FROM PRODUCT";
+    $sql = "SELECT ID , MAIN_PHOTO , PRODUCT_NAME , PRICE , TAG1 , TAG2 FROM PRODUCT";
 
 
     $statement = $pdo->prepare($sql); 
