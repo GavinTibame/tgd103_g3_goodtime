f0f48c8ac032b448ec22d8932c3ac85d3056d0bc
GavinTibame <tibamehui@gmail.com>
2023-01-30 18:53:15 +0800

    20230130在tab2的template中以大div包裹範本

--- php/frontend/checkoutCart.php
+++ php/frontend/checkoutCart.php
@@ -1,5 +1,69 @@
 <?php
 
-print_r($_POST);
+include("../connect.php");
+include("../verify.php");
+
+$mid = isMemberLogin();
+
+$array = $_POST;
+
+$first_key = key($array);
+
+$key = json_decode($first_key, true);
+// print_r($key);
+
+function checkout($dbn, $mid, $pid, $sid){
+
+    $sql = "DELETE FROM CART 
+            WHERE FK_CART_MEMBER_ID = :mid 
+            AND FK_CART_PRODUCT_ID = :pid 
+            AND FK_CART_PRODUCT_SPEC_ID = :sid";
+    
+    $statement = $dbn->prepare($sql);
+    $statement->bindValue(":mid", $mid);
+    $statement->bindValue(":pid", $pid);
+    $statement->bindValue(":sid", $sid);
+    $statement->execute();
+}
+
+function calcAmt($dbn, $mid, $pid, $sid){
+    $amt = 0;
+
+    $sql = "SELECT PRICE, QTY 
+            FROM PRODUCT, CART 
+            WHERE PRODUCT.ID = CART.FK_CART_PRODUCT_ID
+            AND CART.FK_CART_MEMBER_ID = :mid 
+            AND CART.FK_CART_PRODUCT_ID = :pid 
+            AND CART.FK_CART_PRODUCT_SPEC_ID = :sid";
+    
+    $statement = $dbn->prepare($sql);
+    $statement->bindValue(":mid", $mid);
+    $statement->bindValue(":pid", $pid);
+    $statement->bindValue(":sid", $sid);
+    $statement->execute();
+    $charge = $statement->fetchAll();
+    // print_r($charge);
+    $amt = $charge[0]["PRICE"] * $charge[0]["QTY"];
+    return $amt;
+}
+
+$dbn = connectDB(); 
+$dbn->beginTransaction();
+$amt = 0;
+foreach($key as $pid => $sidArray){
+    // print_r($pid);
+    foreach($sidArray as $sid){
+        // print_r($sid);
+        // $amt += calcAmt($dbn, $mid, $pid, $sid);
+        $amt += calcAmt($dbn, 5, $pid, $sid);
+        // checkout($dbn, $mid, $pid, $sid);
+        // checkout($dbn, 5, $pid, $sid);
+    }
+    
+};
+
+$dbn->commit();
+
+print_r($amt);
 
 ?>
--- src/js/component/Tab2Content.js
+++ src/js/component/Tab2Content.js
@@ -1,36 +1,41 @@
 export default {
-  props: { podprops: Object },
+  props: { podProps: Object },
   computed: {
     totalAmt() {
-      console.log(this.podprops);
-      return this.podprops.length;
+      // console.log(this.podprops);
+      return 1;
     }
 
   },
+  mounted() {
+    // console.log(this.podProps);
+  },
   template: `
-    <dl class="podetail">
+    <div>
+    <div class="podetail">
       <div>
-        <dt class="po-key">購買日期</dt>
-        <dd class="po-value">{{podprops}}</dd>
+        <div class="po-key">購買日期</div>
+        <div class="po-value"></div>
       </div>
       <div>
-        <dt class="po-key">主單編號</dt>
-        <dd class="po-value">MO20221210A00001</dd>
+        <div class="po-key">主單編號</div>
+        <div class="po-value">MO20221210A00001</div>
       </div>
       <div>
-        <dt class="po-key">子單編號</dt>
-        <dd class="po-value">MM20221210A00001</dd>
+        <div class="po-key">子單編號</div>
+        <div class="po-value">MM20221210A00001</div>
       </div>
       <div>
-        <dt class="po-key">付款方式</dt>
-        <dd class="po-value">線上刷卡</dd>
+        <div class="po-key">付款方式</div>
+        <div class="po-value">線上刷卡</div>
       </div>
       <div>
-        <dt class="po-key">訂單狀態</dt>
-        <dd class="po-value">訂單已確認</dd>
+        <div class="po-key">訂單狀態</div>
+        <div class="po-value">訂單已確認</div>
       </div>
-    </dl>
-    <dl class="po-item">
+    </div>
+
+    <div class="po-item">
       <div class="item-single">
         <img src="../img/store/store05.jpg" alt="" />
         <div class="item-detail">
@@ -106,20 +111,21 @@ export default {
           </div>
         </div>
       </div>
-    </dl>
-    <dl class="posum">
+    </div>
+    <div class="posum">
         <div>
-          <dt class="po-key">小計：</dt>
-          <dd class="po-value">NT$　{{totalAmt}}</dd>
+          <div class="po-key">小計：</div>
+          <div class="po-value">NT$　{{totalAmt}}</div>
         </div>
         <div>
-          <dt class="po-key">運費：</dt>
-          <dd class="po-value">NT$　　60</dd>
+          <div class="po-key">運費：</div>
+          <div class="po-value">NT$　　60</div>
         </div>
         <div>
-          <dt class="po-key">總金額：</dt>
-          <dd class="po-value">NT$ {{ totalAmt }}</dd>
+          <div class="po-key">總金額：</div>
+          <div class="po-value">NT$ {{ totalAmt }}</div>
         </div>
-      </dl>
+      </div>
+  </div>
   `
 }
